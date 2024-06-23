9d0ec93bf93d0a05a2082d0bf15455d299c360f4
Manu <delafter@gmail.com>
2024-06-23 15:07:48 +0200

    modificado el fetch traer usuario

--- src/store/store.js
+++ src/store/store.js
@@ -1,104 +1,90 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 const useStore = create((set) => ({
-    //variables aqui
-    personajes: [],
-    personaje: {},
-    resultadosBusqueda: [],
-    usuarios: [],
-    usuario: {},
-    
-   
-    
-    getTraerPersonajes: async () => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
-          
-         await fetch("https://rickandmortyapi.com/api/character", requestOptions)
-            .then(response => response.json())
-            .then(data => set({personajes: data.results}))
-            .catch(error => console.log('error', error));
-    },
+  //variables aqui
+  personajes: [],
+  personaje: {},
+  resultadosBusqueda: [],
+  usuarios: [],
+  usuario: {},
 
-   
+  getTraerPersonajes: async () => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
+    await fetch("https://rickandmortyapi.com/api/character", requestOptions)
+      .then((response) => response.json())
+      .then((data) => set({ personajes: data.results }))
+      .catch((error) => console.log("error", error));
+  },
 
-    getTrearUnPersonaje: async (id) => {
-        const requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
+  getTrearUnPersonaje: async (id) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-          
-          
-          await fetch(`https://rickandmortyapi.com/api/character/${id}`, requestOptions)
-            .then(response => response.json())
-            
-            .then(data => set({personaje: data}))
-            .catch(error => console.log('error', error));
-    },
+    await fetch(
+      `https://rickandmortyapi.com/api/character/${id}`,
+      requestOptions
+    )
+      .then((response) => response.json())
 
-    getBuscarPersonaje: async (name) => {
-      const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-  
-      await fetch(`https://rickandmortyapi.com/api/character/?name=${name}`, requestOptions)
-        .then(response => response.json())
-        .then(data => set({ resultadosBusqueda: data.results || [] }))
-        .catch(error => {
-          console.log('error', error);
-          set({ resultadosBusqueda: [] });
-        });
-    },
-  
-    // Traer usuarios
+      .then((data) => set({ personaje: data }))
+      .catch((error) => console.log("error", error));
+  },
 
-    getTraerUsuarios: async () => {
-      const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-  
-      await fetch('http://localhost:3000/api/usuarios', requestOptions)
-        .then(response => response.json())
-        .then(data => set({ usuarios: data }))
-        .catch(error => console.log('error', error));
-    },
+  getBuscarPersonaje: async (name) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-    //crear usuario
+    await fetch(
+      `https://rickandmortyapi.com/api/character/?name=${name}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => set({ resultadosBusqueda: data.results || [] }))
+      .catch((error) => {
+        console.log("error", error);
+        set({ resultadosBusqueda: [] });
+      });
+  },
 
-    getCrearUsuario: async (email, password) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    const raw = JSON.stringify({
-      "email": email,
-      "password": password,
-    });
-    
+  // Traer usuarios
+
+  getTraerUsuarios: async () => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    await fetch("http://localhost:3000/api/usuarios", requestOptions)
+      .then((response) => response.json())
+      .then((data) => set({ usuarios: data }))
+      .catch((error) => console.log("error", error));
+  },
+
+  //crear usuario
+
+  getCrearUsuario: async (email, password) => {
     const requestOptions = {
       method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow"
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+      redirect: "follow",
     };
-    
+
     fetch("http://localhost:3000/api/usuarios", requestOptions)
       .then((response) => response.json())
-      .then((result) => set( {usuario: result}))
+      .then((result) => set({ usuarios: result }))
       .catch((error) => console.error(error));
-    },
-  
-    
-    
-    
-
-
-   
- 
-}))
+  },
+}));
 export default useStore;
